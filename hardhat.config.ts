1e85f7ceee8f4fa7610b7949f8863d5fbbdabe38
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-05 00:32:33 -0300

    Setup react typescript project on its own folder

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import '@nomicfoundation/hardhat-toolbox'
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   paths: {
-    artifacts: './src/artifacts',
+    artifacts: './frontend/src/artifacts',
   },
   networks: {
     hardhat: {
