b18f06afa9b884ac74f7821f1eb75dfc56cbf13a
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-05 00:06:51 -0300

    Added prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+}
 
-export default config;
+export default config
