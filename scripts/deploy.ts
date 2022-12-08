5f34fdb747463f31ba2568f797169064b2194601
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-08 00:18:18 -0300

    Custom token interaction

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,9 +3,15 @@ import { ethers } from 'hardhat'
 async function main() {
   const Greeter = await ethers.getContractFactory('Greeter')
   const greeter = await Greeter.deploy('Hello world')
+
+  const Token = await ethers.getContractFactory('Token')
+  const token = await Token.deploy()
+
   await greeter.deployed()
+  await token.deployed()
 
   console.log('Greeter deployed to:', greeter.address)
+  console.log('Token deployed to:', token.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
