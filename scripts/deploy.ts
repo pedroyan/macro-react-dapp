42d459059d9e92f365f67bd2e920023e622715f3
Pedro Yan Ornelas <pedroyane@gmail.com>
2022-12-08 00:25:14 -0300

    Implemented and deployed ERC20

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,22 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  const Greeter = await ethers.getContractFactory('Greeter')
-  const greeter = await Greeter.deploy('Hello world')
+  const greeterFactory = await ethers.getContractFactory('Greeter')
+  const greeter = await greeterFactory.deploy('Hello world')
 
   const Token = await ethers.getContractFactory('Token')
   const token = await Token.deploy()
 
+  const ndTokenFactory = await ethers.getContractFactory('NDToken')
+  const ndToken = await ndTokenFactory.deploy('NDToken', 'NDT')
+
   await greeter.deployed()
   await token.deployed()
+  await ndToken.deployed()
 
   console.log('Greeter deployed to:', greeter.address)
   console.log('Token deployed to:', token.address)
+  console.log('NDToken deployed to:', ndToken.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
